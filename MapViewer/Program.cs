f766b5331b18bf46496bf8e28a8a9d7535bcf1ad
Константин Бафометов <fofgogjoj@mail.ru>
2022-06-16 16:07:51 +0300

    добавил сервис

--- MapViewer/Program.cs
+++ MapViewer/Program.cs
@@ -1,3 +1,6 @@
+using MapViewer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MapViewerContext>(options =>
+options
+            .UseNpgsql(builder.Configuration.GetConnectionString("MapViewerContext"))
+    );
 
 var app = builder.Build();
 
