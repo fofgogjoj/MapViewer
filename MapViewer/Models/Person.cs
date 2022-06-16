1ae2c755e7a4b969c467ad3d53ea3cb87f44866e
Константин Бафометов <fofgogjoj@mail.ru>
2022-06-16 16:04:20 +0300

    добавил модельки

---
+++ MapViewer/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace MapViewer.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ MapViewer/Models/Place.cs
@@ -0,0 +1,10 @@
+﻿namespace MapViewer.Models
+{
+    public class Place
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
