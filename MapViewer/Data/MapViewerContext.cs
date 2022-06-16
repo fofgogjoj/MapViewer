1af9e70a8c0152b75935dbb22b244c2703f17e3d
Константин Бафометов <fofgogjoj@mail.ru>
2022-06-16 16:05:36 +0300

    добавил контекст данных

---
+++ MapViewer/Data/MapViewerContext.cs
@@ -0,0 +1,16 @@
+﻿using MapViewer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MapViewer
+{
+    public class MapViewerContext : DbContext
+    {
+        public MapViewerContext(DbContextOptions<MapViewerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Place> Places { get; set; }
+    }
+}
