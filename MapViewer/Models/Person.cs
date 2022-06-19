84fa594db8ea345be237ad2113a43760c9b54ff6
Константин Бафометов <fofgogjoj@mail.ru>
2022-06-20 00:53:06 +0300

    добавил модельки

--- MapViewer/Models/Person.cs
+++ MapViewer/Models/Person.cs
@@ -1,10 +1,14 @@
-﻿namespace MapViewer.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MapViewer.Models
 {
     public class Person
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
     }
 }
--- MapViewer/Models/Place.cs
+++ MapViewer/Models/Place.cs
@@ -1,7 +1,12 @@
-﻿namespace MapViewer.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MapViewer.Models
 {
     public class Place
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public string Text { get; set; }
         public double Longitude { get; set; }
